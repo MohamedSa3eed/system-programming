dafdc7db58dd4921d60e5a23366a3a0aed1bd032
MohamedSa3eed <mohamed.sa3ed.faried@gmail.com>
2022-12-06 20:01:12 +0200

    pwd utility : bug fixed

--- femto-unix-utilities/pwd.c
+++ femto-unix-utilities/pwd.c
@@ -9,6 +9,7 @@ int main (int argc, char *argv[])
 	if(argc > 1)
 	{
 		printf("program should not take arguments\n");
+		exit(1);
 	}
 	char *pwd = (char *)malloc(sizeof(char)*MAX_PATH);
 	getcwd(pwd,MAX_PATH);
