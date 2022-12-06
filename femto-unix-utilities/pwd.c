de0de72f64085048a05e545d0be102711f9f98c7
MohamedSa3eed <mohamed.sa3ed.faried@gmail.com>
2022-12-06 19:51:11 +0200

    pwd utility done

---
+++ femto-unix-utilities/pwd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MAX_PATH 4096
+
+int main (int argc, char *argv[])
+{
+	if(argc > 1)
+	{
+		printf("program should not take arguments\n");
+	}
+	char *pwd = (char *)malloc(sizeof(char)*MAX_PATH);
+	getcwd(pwd,MAX_PATH);
+	printf("%s\n",pwd);
+	return 0;
+}
